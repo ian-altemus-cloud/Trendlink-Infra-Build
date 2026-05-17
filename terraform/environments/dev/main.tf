e9e3b083db555b1b56694efcad5285ebef95ea89
Ian Altemus <YOUR_EMAIL_HERE>
2026-05-17 15:14:25 -0700

    feat: add DynamoDB lock table for dev Terraform state

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -256,3 +256,20 @@ resource "aws_s3_bucket_policy" "tfstate" {
     ]
   })
 }
+
+resource "aws_dynamodb_table" "tfstate_lock" {
+  name         = "tl-tfstate-dev-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "tl-tfstate-dev-lock"
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
+}
